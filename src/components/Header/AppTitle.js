ad8aea6935d4e80a4212dde3b9e6ed0a50d9edc5
Julian Ehiem <jules.ehiem@gmail.com>
2021-02-12 07:02:23 -0500

    added bg-image for title

--- src/components/Header/AppTitle.js
+++ src/components/Header/AppTitle.js
@@ -1,10 +1,12 @@
 import "./appTitle.css";
 import NewButton from "../Buttons/NewButton";
+import logo from "../../crumpled.png"
 
 export default function AppTitle() {
   return (
     <div className="titleContainer">
       <h1 className="title">KWIKNOTE</h1>
+      {/* <img src={logo} alt="kwiknote logo"/> */}
       <NewButton />
     </div>
   );
